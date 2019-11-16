acfb0ff7edf3ea7fd931bcf6e154b39636916641
obaydullah <obaydullah599@gmail.com>
2019-11-16 23:22:12 +0600

    Second commit

---
+++ DevskillAssignment/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1
+{
+    class Circle
+    {
+        private string color;
+        private double radius;
+
+        public Circle()
+        {
+            radius = 1.5;
+            color = "red";
+        }
+        public Circle(double r)
+        {
+            radius = r;
+            color = "red";
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+        private double CalculateArea()
+        {
+            return 3.14 * radius * radius;
+        }
+        public string circleDetails()
+        {
+            string message = "Radius: " + this.radius + "\nColor: " + this.color + "\nArea :" + this.CalculateArea();
+            return message;
+        }
+
+    }
+
+  }
+
