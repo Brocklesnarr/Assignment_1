b792d04719c5672f4d523b249f16d5614d85d021
obaydullah <obaydullah599@gmail.com>
2019-11-16 22:58:36 +0600

    First commit

---
+++ DevskillAssignment/Assignment_1_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Radius: ");
+            double rad =Convert.ToDouble(Console.ReadLine());
+            Circle circle = new Circle(rad);
+            Console.WriteLine("\n");
+            Console.WriteLine(circle.circleDetails());
+            Console.ReadKey();
+        }
+    }
+}
